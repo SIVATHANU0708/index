e57f21e49f7feef4bcd618356fdfceb7b6888244
SIVATHANU0708 <35222125+SIVATHANU0708@users.noreply.github.com>
2018-02-12 16:49:37 +0530

    index.C
    index of an element

---
+++ index.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n,i,a[i];
+printf("enter the no of elemnt:");
+scanf("%d",&n);
+printf("Enter the element %d:",n);
+for(i=0;i<=n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<=n;i++)
+{
+printf("%d\t %d\n",i,a[i]);
+}
+return 0;
+}
